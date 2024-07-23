fc66ad63d6c8c4a9782c5d0b4c3c582973d82f53
Sahin Nayak <89744970+Sahin-Nayak@users.noreply.github.com>
2024-07-23 19:06:06 +0530

    Add files via upload

---
+++ sem3/python/Assignment 1/q1_convert_c_to_f.py
@@ -0,0 +1,2 @@
+temp = int(input("Enter Temperature: "))
+print("Temperature in Celsius: " + str((9 / 5) * temp + 32))
---
+++ sem3/python/Assignment 1/q7_divisile_by_7_but_not_by_5.py
@@ -0,0 +1,10 @@
+def isDivisible(n):
+    if not n % 7 and n % 5:
+        return True
+    return False
+
+res = []
+for i in range(1000, 2001):
+    if isDivisible(i):
+        res.append(i)
+print(res)
