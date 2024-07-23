0683e04c337c3f2eebcd41f36d007d8251df4125
Sahin Nayak <89744970+Sahin-Nayak@users.noreply.github.com>
2024-07-23 19:41:38 +0530

    Add files via upload

---
+++ sem2/java/code/week7/Java_7/Q_22.java
@@ -0,0 +1,14 @@
+class NotPositiveException extends Exception {
+    public NotPositiveException(String message) {
+        super(message);}}
+ class Q_22 {
+    public static void checkNumber(int number) throws NotPositiveException {
+        if (number <= 0) {
+            throw new NotPositiveException("Number is not positive.");
+        } else {
+            System.out.println(number + " is a positive number.");}}
+    public static void main(String[] args) {
+        try {
+            checkNumber(-5); 
+        } catch (NotPositiveException e) {
+            System.out.println("Caught Exception: " + e.getMessage());}}}
---
+++ sem2/java/code/week7/Java_7/Q_4.java
@@ -0,0 +1,19 @@
+public class Q_4 {
+    private int totalAlphabeticCharacters;
+    public Q_4() {
+        this.totalAlphabeticCharacters = 0;}
+    public void addCharacter(char c) throws IllegalArgumentException {
+        if (!Character.isLetter(c)) {
+            throw new IllegalArgumentException("Non-alphabetic character passed: " + c);}
+        totalAlphabeticCharacters++;}
+    public int getTotalAlphabeticCharacters() {
+        return totalAlphabeticCharacters;}
+    public static void main(String[] args) {
+        Q_4 counter = new Q_4();
+        try {
+            counter.addCharacter('a');
+            counter.addCharacter('b');
+            counter.addCharacter('1'); // This will throw an exception
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());}
+        System.out.println("Total alphabetic characters: " + counter.getTotalAlphabeticCharacters());}}
